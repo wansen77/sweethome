8b66e74db4d12f92f1f4bfae80b719d932f8f04a
WangsenLin <linwangsen77@gmail.com>
2019-12-04 22:27:54 +0800

    -daddenv

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/eric-project/'
-    : '/',
   API_PATH: '"https://vue-course-api.hexschool.io"',
   CUSTOM_PATH: '"wansen77"'
 }
