6c148d2f28aa4068fdb3a684aef42ab205dc53c4
WangsenLin <linwangsen77@gmail.com>
2019-12-04 16:28:05 +0800

    updetaplugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
+import 'bootstrap'
 
+axios.defaults.withCredentials = true
+// 跨域必須加上這段且在login上的API必須使用 / admin / signin
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
